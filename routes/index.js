98f24cccef5d11f93288f770e52b079fadca2a44
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-23 11:26:15 -0500

    install passport, session, and oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+router.get('/auth/google', passport.authenticate(
+    'google', { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+    'google', {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+));
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.render('index');
 });
 
 module.exports = router;
